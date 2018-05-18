408383d8235b77443369127bbab300e3e28769f2
Chris <orthagonal1@gmail.com>
2018-05-18 14:40:59 -0500

    fixes

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const register = (server, pluginOptions) => {
     const tableAttributes = options && options.tableAttributes ? ` ${options.tableAttributes}` : '';
     const header = `<thead>${os.EOL}<tr><th>${table[0].join('</th><th>')}</th></tr>${os.EOL}</thead>${os.EOL}`;
     const rows = table.slice(1).reduce((tableString, n) => `${tableString}<tr><td>${n.join('</td><td>')}</td></tr>${os.EOL}`, '');
-    const css = options.css.map(link => `<link rel="stylesheet" type="text/css" href=${link}>"`).join(os.EOL);
+    const css = options.css.map(link => `<link rel="stylesheet" type="text/css" href=${link}>`).join(os.EOL);
     const scripts = options.scripts.map(link => `<script type="text/javascript" src="${link}"></script>`).join(os.EOL);
     return `${css}${css ? os.EOL : ''}${scripts}${scripts ? os.EOL : ''}<table${tableAttributes}>${os.EOL}${header}${os.EOL}<tbody>${rows}</tbody>${os.EOL}</table>`;
   };
@@ -33,9 +33,9 @@ const register = (server, pluginOptions) => {
       options.css = options.css || [];
       options.scripts = options.scripts || [];
       if (options.datatable) {
-        options.css.push('http://cdn.datatables.net/1.10.16/css/jquery.dataTables.min.css');
+        options.css.push('https://cdn.datatables.net/1.10.16/css/jquery.dataTables.min.css');
         options.scripts.push('https://code.jquery.com/jquery-3.3.1.min.js');
-        options.scripts.push('http://cdn.datatables.net/1.10.16/js/jquery.dataTables.min.js');
+        options.scripts.push('https://cdn.datatables.net/1.10.16/js/jquery.dataTables.min.js');
         options.tableAttributes = 'id="table"';
       }
       let tableString = tableToHtml(jsonToTable(source, options), options);
