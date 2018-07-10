d9115ba592e334de008f140d929afa56bf66c77d
orthagonal <orthagonal1@gmail.com>
2018-07-10 22:31:41 +0000

    suport nested object

--- tests/test.html.js
+++ tests/test.html.js
@@ -88,7 +88,7 @@ tap.test('can configure a route to return html table instead of json', async(t) 
   t.end();
 });
 
-tap.test('can render configure a route to return html table instead of json', async(t) => {
+tap.test('can render a single object as html table', async(t) => {
   const server = await new Hapi.Server({ port: 8080 });
   server.route({
     method: 'get',
@@ -110,6 +110,38 @@ tap.test('can render configure a route to return html table instead of json', as
   t.equal(tableResponse.statusCode, 200, 'returns HTTP OK');
   t.equal(typeof tableResponse.result, 'string', 'returns a string value');
   t.equal(tableResponse.result, fs.readFileSync(path.join(__dirname, 'outputObject.html'), 'utf-8'), 'produces correct HTML output');
+  await server.stop();
+  t.end();
+});
+
+tap.test('can render a nested  object as html table', async(t) => {
+  const server = await new Hapi.Server({ port: 8080 });
+  server.route({
+    method: 'get',
+    path: '/normal',
+    handler(request, h) {
+      return {
+        subObj: {
+          car: 'Audi',
+          price: 40000,
+          color: {
+            r: 255,
+            g: 12,
+            b: 55
+          }
+        }
+      };
+    }
+  });
+  await server.register(plugin, {});
+  await server.start();
+  const tableResponse = await server.inject({
+    method: 'get',
+    url: '/normal.html'
+  });
+  t.equal(tableResponse.statusCode, 200, 'returns HTTP OK');
+  t.equal(typeof tableResponse.result, 'string', 'returns a string value');
+  t.equal(tableResponse.result, fs.readFileSync(path.join(__dirname, 'outputFormat.html'), 'utf-8'), 'produces correct HTML output');
   await server.stop();
   t.end();
 });
